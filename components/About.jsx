596f46d1a570f334327336b8e6c111285acf103b
Tahsin Alamgir Kheya <tahsinkheya@gmail.com>
2024-06-13 22:08:46 +1000

    changed about me

--- components/About.jsx
+++ components/About.jsx
@@ -18,26 +18,15 @@ const About = () => {
           Hi there! My name is Kheya.
         </h1>
         <p className="py-5 sm:text-2xl text-xl ">
-          {/* <RoughNotationGroup show={window.innerWidth < 1060 ? false : true}> */}
-            {/* I take pleasure in crafting{" "} */}
-            {/* <RoughNotation type="highlight" color="#a86561" order="1"> */}
-            {/* uncomplicated yet elegant */}
-             {/* </RoughNotation>{" "} */}
-            {/* creations for both business and individual use. */}
-            I am a researcher, currently working on AI/ML bias. My goal is to develop tools to detect and mitigate bias in real-world applications.
-            From my standpoint, technology should be equitable and inclusive. I am enthusiatic about contributing in this area to make it a reality.
-             Moreover I am a passionate front-end engineer who is constantly
-            looking for opportunities to apply my expertise in{" "}
-            {/* <RoughNotation type="highlight" color="#c09591" order="2"> */}
-              web development
-            {/* </RoughNotation>{" "} */}
-            to produce visually appealing and{" "}
-            {/* <RoughNotation type="highlight" color="#55BEC0" order="3"> */}
-              user-friendly
-            {/* </RoughNotation> */}
-          {/* </RoughNotationGroup>{" "} */}
-          applications and websites. In my free time, I like to play Genshin
-          Impact, watch movies, and doodle .
+          I am a researcher, currently working on AI/ML bias. My goal is to
+          develop tools to detect and mitigate bias in real-world applications.
+          From my standpoint, technology should be equitable and inclusive. I am
+          enthusiastic about contributing in this area to make it a reality.
+          Moreover I am a passionate front-end engineer who is constantly
+          looking for opportunities to apply my expertise in web development to
+          produce visually appealing and user-friendly applications and
+          websites. In my free time, I like to play Genshin Impact, watch
+          movies, and doodle.
         </p>
         <a
           href=""
@@ -50,20 +39,18 @@ const About = () => {
       </div>
       <div className=" grid place-items-center md:grid place-items-left ">
         <div className="bg-transparent">
-
-      <iframe src="https://drive.google.com/file/d/1xTxEwdaWWwrCWbgh3imWOnCQWaH-g0ke/preview" width="300" height="325" className="rounded-full bg-transparent" ></iframe>
-
+          <iframe
+            src="https://drive.google.com/file/d/1xTxEwdaWWwrCWbgh3imWOnCQWaH-g0ke/preview"
+            width="300"
+            height="325"
+            className="rounded-full bg-transparent"
+          ></iframe>
         </div>
-      {/* <img src="https://drive.google.com/uc?id=1xTxEwdaWWwrCWbgh3imWOnCQWaH-g0ke" alt="Description of the image"/> */}
-      {/* // "https://s3.brilliant.com.bd/tahsin/kheyaPortfolio/WhatsApp%20Image%202023-05-28%20at%209.46.19%20AM.jpeg"
+        {/* <img src="https://drive.google.com/uc?id=1xTxEwdaWWwrCWbgh3imWOnCQWaH-g0ke" alt="Description of the image"/> */}
+        {/* // "https://s3.brilliant.com.bd/tahsin/kheyaPortfolio/WhatsApp%20Image%202023-05-28%20at%209.46.19%20AM.jpeg"
 
           // className="h-[370px] w-[400px] rounded-full" */}
-          {/* /> */}
-
-      
-       
-            
-        
+        {/* /> */}
       </div>
     </div>
     // </ScrollAnimation>
